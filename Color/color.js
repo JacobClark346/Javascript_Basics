d03d2a1b03abd70baf9297c4fbddce11a8d8b7d6
JacobClark346 <jacobclark961@gmail.com>
2020-12-15 19:21:42 +0000

    colors done

---
+++ Color/color.js
@@ -0,0 +1,10 @@
+const name = prompt ("Hello! What is your name?")
+const color = prompt ("Choose a color. Red, Green, or Yellow")
+
+if (color == "red") {
+    alert("You chose the color RED")
+} else if (color == "green") {
+    alert("You chose the color GREEN")
+} else if (color == "yellow") {
+    alert("You chose the color YELLOW")
+}
