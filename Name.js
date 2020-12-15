e04f670af981904150ef3305ff3b202d12ec9973
JacobClark346 <72883957+JacobClark346@users.noreply.github.com>
2020-12-15 14:28:52 -0500

    Delete Name.js

--- Name.js
+++
@@ -1,6 +0,0 @@
-const name = prompt ("Hi, what is your name?")
-const work = prompt ("Hi, " + name + "! What do you do for a living?")
-const opinion = prompt ("Interesting! Do you love being a " + work.toLowerCase() + "?")
-if (opinion == "yes") {
-  console.log (name + " is a " + work.toLowerCase() + " and loves it")
-} 
